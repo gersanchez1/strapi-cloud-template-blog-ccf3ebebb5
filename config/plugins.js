0ad1c24ba632b857eced9fc705b9294643c91e30
gersanchez1 <german.sanchez@infracommerce.lat>
2026-04-15 14:14:49 -0300

    fix: config maxage y cache

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,19 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+  'rest-cache': {
+    config: {
+      provider: {
+        name: 'memory', // O 'redis' para mayor estabilidad
+        options: {
+          max: 32767,
+          maxAge: 3600000, // 1 hora en milisegundos (ajusta según necesites)
+        },
+      },
+      strategy: {
+        contentTypes: [
+          // Lista aquí los UIDs de tus componentes o colecciones
+          'api::articulo.articulo', 
+        ],
+      },
+    },
+  },
+});
